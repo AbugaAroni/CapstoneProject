4f934932099aae56c385353e05c6f8409d89fbfa
AbugaAroni <abugaaroni@gmail.com>
2020-11-03 15:38:47 +0300

    add education class to model

--- api/models.py
+++ api/models.py
@@ -2,5 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Q
 
-
 # Create your models here.
+class education(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    start = models.DateField(auto_now=False)
+    finish = models.DateField(auto_now=False)
+
+    def __str__(self):
+        return self.title
