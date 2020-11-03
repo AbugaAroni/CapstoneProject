45bcf98876594d0479d0098ceb3656fd35063410
AbugaAroni <abugaaroni@gmail.com>
2020-11-03 15:54:45 +0300

    add working tests for job model

--- api/tests.py
+++ api/tests.py
@@ -1,12 +1,19 @@
 from django.test import TestCase
 from django.db import models
-from .models import education
+from .models import education, job
 
 class EducationTestClass(TestCase):
         # Set up method
     def setUp(self):
-        self.abugaeducate = education(title="rick", description="my first degree", start = '2020-10-20', finish='2020-10-21')
+        self.abugaeducate = education(title="rick", description="my first degree", qualification = "undegraduate",start = '2020-10-20', finish='2020-10-21')
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.abugaeducate,education))
 # Create your tests here.
+class JobTestClass(TestCase):
+        # Set up method
+    def setUp(self):
+        self.abugajob = job(title="rick", description="my first degree",start = '2020-10-20', finish='2020-10-21')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.abugajob,job))
