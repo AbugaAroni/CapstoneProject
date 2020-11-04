16824d3e10778769d4e74f6ef96fcfca14f96040
AbugaAroni <abugaaroni@gmail.com>
2020-11-04 15:49:45 +0300

    add test for project class

--- api/tests.py
+++ api/tests.py
@@ -63,3 +63,22 @@ class ContactTestClass(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.abugacontact,contact))
+
+class ProjectTestClass(TestCase):
+        #set up method
+    def setUp(self):
+        self.abugapics = pictures(title="rick", alt_tag="my first degree", image = "xyz")
+        self.abugajob = job(title="rick", description="my first degree",start = '2020-10-20', finish='2020-10-21')
+        self.abugaeducate = education(title="cambridge", description="my second degree", qualification = "undegraduate",start = '2020-10-20', finish='2020-10-21')
+        self.abuga = User(username="rick", password="password")
+        self.abugauser = user(user=self.abuga, first_name="Rick", last_name="ricardo", admin=True,about="Abuga is not rick")
+        self.abuga.save()
+        self.abugapics.save()
+        self.abugajob.save()
+        self.abugaeducate.save()
+        self.abugauser.save()
+
+        self.abugaproject = project(writer=self.abugauser, title="This is my firs project", description="blah blah blah blah", live_link="github.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.abugaproject,project))
