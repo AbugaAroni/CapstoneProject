25cde362d06b037cbea72b8aa3effdfd62ef8d35
AbugaAroni <abugaaroni@gmail.com>
2020-11-04 22:35:25 +0300

    add contact, project and blog_post serilizers to the views

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import user, education, job, pictures,  contact, project, blog_post
+from .models import user, education, job, pictures, contact, project, blog_post
 from django.contrib.auth.models import User
-from .serializer import userSerializer, EducationSerializer, JobSerializer, PicturesSerializer
+from .serializer import userSerializer, EducationSerializer, JobSerializer, PicturesSerializer, ProjectSerializer, Blog_PostSerializer, ContactSerializer
 
 class EducationView(viewsets.ModelViewSet):
     queryset = education.objects.all()
@@ -19,3 +19,15 @@ class PicturesView(viewsets.ModelViewSet):
 class usersView(viewsets.ModelViewSet):
     queryset = user.objects.all()
     serializer_class = userSerializer
+
+class ProjectView(viewsets.ModelViewSet):
+    queryset = project.objects.all()
+    serializer_class = ProjectSerializer
+
+class Blog_PostView(viewsets.ModelViewSet):
+    queryset = blog_post.objects.all()
+    serializer_class = Blog_PostSerializer
+
+class ContactView(viewsets.ModelViewSet):
+    queryset = contact.objects.all()
+    serializer_class = ContactSerializer
