443935183c13c267ff09be17b28e18c2e543d8e0
AbugaAroni <abugaaroni@gmail.com>
2020-11-04 23:52:25 +0300

    add changes to make heroku work

--- abugaaronico/settings.py
+++ abugaaronico/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
 import os
+from pathlib import Path
 import django_heroku
 import dj_database_url
 from decouple import config,Csv
