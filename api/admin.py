349ba0faed211113c00adeea02ca69d8aa31d5db
AbugaAroni <abugaaroni@gmail.com>
2020-11-04 16:05:12 +0300

    configure admin and rest_framework installs

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import education, job, pictures, user, contact, project, blog_post
 
 # Register your models here.
+admin.site.register(education)
+admin.site.register(job)
+admin.site.register(pictures)
+admin.site.register(user)
+admin.site.register(contact)
+admin.site.register(blog_post)
+admin.site.register(project)
