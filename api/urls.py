36ca146b718d56dbeedf1295f29a8954dbd3e708
AbugaAroni <abugaaroni@gmail.com>
2020-11-04 22:36:00 +0300

    add contact, project and blog_post views to router in urls.py

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,9 @@ router.register('education', views.EducationView)
 router.register('jobs', views.JobView)
 router.register('pictures', views.PicturesView)
 router.register('user', views.usersView)
+router.register('contact', views.ContactView)
+router.register('project', views.ProjectView)
+router.register('blog_post', views.Blog_PostView)
 
 urlpatterns=[
 path('', include(router.urls))
