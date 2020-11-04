2a7435d1afa99d09501ac5499600db1a410205cd
AbugaAroni <abugaaroni@gmail.com>
2020-11-04 15:53:50 +0300

    add test class for blog_posts

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.db import models
-from .models import education, job, pictures, user, contact, project
+from .models import education, job, pictures, user, contact, project, blog_post
 from django.contrib.auth.models import User
 
 # Create your tests here.
@@ -48,15 +48,9 @@ class UserTestClass(TestCase):
 class ContactTestClass(TestCase):
         #set up method
     def setUp(self):
-        self.abugapics = pictures(title="rick", alt_tag="my first degree", image = "xyz")
-        self.abugajob = job(title="rick", description="my first degree",start = '2020-10-20', finish='2020-10-21')
-        self.abugaeducate = education(title="cambridge", description="my second degree", qualification = "undegraduate",start = '2020-10-20', finish='2020-10-21')
         self.abuga = User(username="rick", password="password")
         self.abugauser = user(user=self.abuga, first_name="Rick", last_name="ricardo", admin=True,about="Abuga is not rick")
         self.abuga.save()
-        self.abugapics.save()
-        self.abugajob.save()
-        self.abugaeducate.save()
         self.abugauser.save()
 
         self.abugacontact = contact(user=self.abugauser, email="Rick@gmail.com", phone="070746346",)
@@ -68,17 +62,28 @@ class ProjectTestClass(TestCase):
         #set up method
     def setUp(self):
         self.abugapics = pictures(title="rick", alt_tag="my first degree", image = "xyz")
-        self.abugajob = job(title="rick", description="my first degree",start = '2020-10-20', finish='2020-10-21')
-        self.abugaeducate = education(title="cambridge", description="my second degree", qualification = "undegraduate",start = '2020-10-20', finish='2020-10-21')
         self.abuga = User(username="rick", password="password")
         self.abugauser = user(user=self.abuga, first_name="Rick", last_name="ricardo", admin=True,about="Abuga is not rick")
         self.abuga.save()
         self.abugapics.save()
-        self.abugajob.save()
-        self.abugaeducate.save()
         self.abugauser.save()
 
         self.abugaproject = project(writer=self.abugauser, title="This is my firs project", description="blah blah blah blah", live_link="github.com")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.abugaproject,project))
+
+class Blog_postTestClass(TestCase):
+        #set up method
+    def setUp(self):
+        self.abugapics = pictures(title="rick", alt_tag="my first degree", image = "xyz")
+        self.abuga = User(username="rick", password="password")
+        self.abugauser = user(user=self.abuga, first_name="Rick", last_name="ricardo", admin=True,about="Abuga is not rick")
+        self.abuga.save()
+        self.abugapics.save()
+        self.abugauser.save()
+
+        self.abugablog_post = blog_post(writer=self.abugauser, title="This is my firs project", content="blah blah blah blah")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.abugablog_post,blog_post))
